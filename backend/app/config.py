7fe436e6af14a0ea9d9570a628bc75d7ff4b7ceb
Ermes Liriano <ermesliriano@mercedarios.net>
2026-04-01 11:24:10 +0200

    Add default values to config settings
    Set default values for JWT_SECRET and GOOGLE_WEB_CLIENT_ID.

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,12 +6,12 @@ class Settings(BaseSettings):
     ENV: str = "dev"
     DATABASE_URL: str = "sqlite:///./dev.db"
 
-    JWT_SECRET: str
+    JWT_SECRET: str = "6d93dda4da9ed79772d4b3dda0a072ac448ef4ad3ac7de1de09d15ec8841b26f"
     JWT_ALG: str = "HS256"
     ACCESS_TOKEN_MINUTES: int = 15
     REFRESH_TOKEN_DAYS: int = 14
 
-    GOOGLE_WEB_CLIENT_ID: str  # el "client_id" de OAuth 2.0 (Web application)
+    GOOGLE_WEB_CLIENT_ID: str  = "423467785550-6qeuu5n702rvbpumf6tl8mqe5gs4obh6.apps.googleusercontent.com" # el "client_id" de OAuth 2.0 (Web application)
     CORS_ORIGINS: str = "https://ermesliriano.github.io"
 
 settings = Settings()
